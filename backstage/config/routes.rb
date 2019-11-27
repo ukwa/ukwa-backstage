cd5bdea98d4f112152f5813089da3bb81dae2168
Andrew Jackson <Andrew.Jackson@bl.uk>
2019-11-27 11:00:46 +0000

    Switch back to using catalog controller directly.

--- backstage/config/routes.rb
+++ backstage/config/routes.rb
@@ -2,16 +2,16 @@ Rails.application.routes.draw do
 
   mount Blacklight::Engine => '/'
   concern :marc_viewable, Blacklight::Marc::Routes::MarcViewable.new
-  root to: "tracking#index"
+  root to: "catalog#index"
   concern :searchable, Blacklight::Routes::Searchable.new
 
-  resource :tracking, only: [:index], as: 'tracking', path: '/tracking', controller: 'tracking' do
+  resource :catalog, only: [:index], as: 'catalog', path: '/catalog', controller: 'catalog' do
     concerns :searchable
   end
   devise_for :users
   concern :exportable, Blacklight::Routes::Exportable.new
 
-  resources :solr_documents, only: [:show], path: '/tracking', controller: 'tracking' do
+  resources :solr_documents, only: [:show], path: '/catalog', controller: 'catalog' do
     concerns [:exportable, :marc_viewable]
   end
 
